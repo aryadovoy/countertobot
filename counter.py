e9df6f17a921f5e4fc698d586240a0bbc844c5c4
aryadovoy <ryav.omsk@gmail.com>
2021-05-29 23:57:26 +0400

    divided by funcs and cutted the answers

--- counter.py
+++ counter.py
@@ -7,29 +7,41 @@ dp = Dispatcher(bot)
 format = '%Y-%m-%d'
 
 
+async def ctd(a):
+    second_date = datetime.datetime.strptime(a[-10:],format)
+    first_date = datetime.datetime.today()
+    diff_dates = second_date - first_date + datetime.timedelta(days=1)
+    return str(diff_dates.days)
+
+
+async def cbd(a):
+    second_date = datetime.datetime.strptime(a[-10:],format)
+    first_date = datetime.datetime.strptime(a[-21:-11],format)
+    diff_dates = second_date - first_date
+    return str(diff_dates.days)
+
+
 @dp.message_handler(commands=['start'])
 async def process_start_command(msg: types.Message):
-    await msg.reply('Hello!\nI was made for counting days, use `/counttodate` for count from today to date and `/countdiff` for count between two dates. Use yyyy-mm-dd format.', parse_mode='markdown')
+    await msg.reply('Hello!\nI was made for counting days, use `/ctd` for count from today to date and `/cbd` for count between two dates. Use yyyy-mm-dd format.', parse_mode='markdown')
+
 
 @dp.message_handler(commands=['help'])
 async def process_help_command(msg: types.Message):
-    await msg.reply('Write `/counttodate yyyy-mm-dd` or `/countdiff yyyy-mm-dd yyyy-mm-dd`.', parse_mode='markdown')
+    await msg.reply('Write `/ctd yyyy-mm-dd` or `/cbd yyyy-mm-dd yyyy-mm-dd`.', parse_mode='markdown')
 
-@dp.message_handler(commands=['counttodate'])
-async def get_message(msg: types.Message):
-    second_date = datetime.datetime.strptime(msg.text[-10:],format)
-    first_date = datetime.datetime.today()
-    td = first_date.strftime('Today is %A, %d %B, %Y.')
-    diff_dates = second_date - first_date + datetime.timedelta(days=1)
-    await msg.answer(td + ' To ' + msg.text[-10:] + ' is ' + str(diff_dates.days) + ' days.')
 
-@dp.message_handler(commands=['countdiff'])
+@dp.message_handler(commands=['ctd'])
 async def get_message(msg: types.Message):
-    second_date = datetime.datetime.strptime(msg.text[-10:],format)
-    first_date = datetime.datetime.strptime(msg.text[-21:-11],format)
-    td = first_date.strftime('First day is %A, %d %B, %Y.')
-    diff_dates = second_date - first_date
-    await msg.answer(td + ' To ' + msg.text[-10:] + ' is ' + str(diff_dates.days) + ' days.')
+    ans = await ctd(msg.text)
+    await msg.answer(ans)
+
+
+@dp.message_handler(commands=['cbd'])
+async def get_message(msg: types.Message):
+    ans = await cbd(msg.text)
+    await msg.answer(ans)
+
 
 @dp.message_handler(types.ChatType.is_private)
 async def wrong_format(msg: types.Message):
